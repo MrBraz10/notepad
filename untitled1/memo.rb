869edf69c1efb3894f85c328d25f25aef4a7341f
GaripbI4 <nekita10@mail.ru>
2022-08-23 21:44:52 +0300

    Nitepad v.1 working

--- untitled1/memo.rb
+++ untitled1/memo.rb
@@ -1,10 +1,22 @@
 class Memo < Post
 
   def read_from_console
-    super
+    puts "Новая заметка (все, что пишите до строчки \"end\"):"
+
+    @text = []
+    line = nil
+
+    while line != "end" do
+      line = STDIN.gets.chomp
+      @text << line
+    end
+
+    @text.pop
   end
 
   def to_strings
-    super
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    return @text.unshift(time_string)
   end
 end
--- untitled1/task.rb
+++ untitled1/task.rb
@@ -1,7 +1,26 @@
+require 'date'
 class Task < Post
   def initialize
     super
 
     @due_date = Time.now
+  end
+
+  def read_from_console
+    puts "Что надо делать?"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, например 12.05.2022"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
+  end
+
+  def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    deadline = "Крайник срок: #{@due_date}"
+
+    return [deadline, @text, time_string]
   end
 end
